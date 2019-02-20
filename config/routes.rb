0e9495095e6888f7e64843214741051ed0ab1f79
EC2 Default User <ec2-user@ip-172-31-15-240.eu-west-1.compute.internal>
2019-02-20 07:34:20 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :items
+  root 'static_pages#home'
+  
+  get '/about' => 'static_pages#about'
+  get '/help' => 'static_pages#help'
+  
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
